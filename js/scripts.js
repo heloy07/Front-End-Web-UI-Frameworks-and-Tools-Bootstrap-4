bcdcc59a50bf223128230d539e279181200cf072
Heloy <heloy.leal@hotmail.com>
2020-05-25 22:57:28 +0200

    onchange and parallelshell

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('#mycarousel').carousel({
+        interval: 2000,
+        cycle: true,
+        pause: "null"
+    });
+    //data-toggle for #loginModal'
+    $('#loginButton').click(function(){
+        $('#loginModal').modal('toggle');
+    });
+    //data-toggle for #reserveModal'
+    $('#reserveButton').click(function(){
+        $('#reserveModal').modal('toggle');
+    });
+    
+
+    
+
+    $('#carouselButton').click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        
+        }
+    });
+   
+
+}); 
